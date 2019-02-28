cf68eb05f2adeacfe32e2fd156d3684e3b2d15d8
unknown <sergeymailmobile@gmail.com>
2019-02-28 21:15:39 +0300

    Test1

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string _surname;
+        private readonly int _age;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            _surname = surname;
+            _age = age;
+        }
+
+        private string PrintAge()
+        {
+            return _age.ToString();
+        }
+
+        public void ChangeSurname(string surname)
+        {
+            _surname = surname;
+        }
+
+        public string EmployeeInfo()
+        {
+            return $"Surname:{_surname},Age:{_age}";
+        }
+
+    }
+}
